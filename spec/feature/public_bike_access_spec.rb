e4c8b78721a02bc9ca02f8cfd28813cfe20237b8
Jade King <dev@jadeking.co.uk>
2015-03-17 16:03:24 +0000

    fixed camel casing in test & added require

--- spec/feature/public_bike_access_spec.rb
+++ spec/feature/public_bike_access_spec.rb
@@ -1,8 +1,9 @@
 require 'capybara/rspec'
+require 'docking_station'
 
 feature 'member of the public accesses bike' do
   scenario 'docking station releases a bike that is not broken' do
-    docking_station = Docking_station.new
+    docking_station = DockingStation.new
     bike = docking_station.release_bike
     expect(bike).not_to be_broken
   end
